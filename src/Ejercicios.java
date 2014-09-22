89bc1a13bb8b432bba3fcc27637d113900108734
elyh31 <elyh31@unitec.edu>
2014-09-22 07:57:06 -0600

    tarea 4

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -4,12 +4,14 @@ public class Ejercicios {
 	//Devuelve el valor almacenado en el indice posicion
 	static int obtenerValor(int arreglo[], int posicion)
 	{
+		//me devuelve el arreglo segun la posicion
 		return arreglo[posicion];
 	}
 	
 	//Devuelve el valor almacenado en el indice posicion
 	static String obtenerValor(String arreglo[], int posicion)
 	{
+		//me devuelve el arreglo segun la posicion
 		return arreglo[posicion];
 	}
 	
@@ -17,10 +19,13 @@ public class Ejercicios {
 	static int obtenerSuma(int arreglo[])
 	{
 		int suma = 0;
+		//el ciclo corre todo el arreglo
 		for (int x=0; x<arreglo.length; x++)
 		{
+			//este es para ir sumando el valor de cada arreglo
 			suma = suma + arreglo[x];
 		}
+		//devuelve la suma total de todo el arreglo
 		return suma;
 	}
 	
@@ -28,36 +33,45 @@ public class Ejercicios {
 	static int obtenerPromedio(int arreglo[])
 	{
 		int suma = 0;
+		//el ciclo corre todo el arreglo
 		for (int x=0; x<arreglo.length; x++)
 		{
+			//este es para ir sumando el valor de cada arreglo
 			suma = suma + arreglo[x];
 		}
+		//divide la suma de los valores de todos los arreglos por la cantidad de arreglos existentes
 		return suma / arreglo.length;
 	}
 	
 	//Devuelve true si valor esta 
 	static boolean existe(char arreglo[], char valor)
 	{
+		//el ciclo corre todo el arreglo
 		for (int x=0; x<arreglo.length; x++)
 		{
+			// me dici verdadero si el valos existe en algun arreglo
 			if (arreglo[x] == valor)
 			{
 				return true;
 			}
 		}
+		// me dici falso si el valos no existe en algun arreglo
 		return false;
 	}
 		
 	//Devuelve true si valor esta 
 	static boolean existe(String arreglo[], String valor)
 	{
+		//el ciclo corre todo el arreglo
 		for (int x=0; x<arreglo.length; x++)
 		{
+			// me dici verdadero si el valos existe en algun arreglo
 			if (arreglo[x].equals(valor))
 			{
 				return true;
 			}
 		}
+		// me dici falso si el valos no existe en algun arreglo
 		return false;
 	}
 	
